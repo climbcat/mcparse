2c54360fca2d0bffd30d5a379649221ef51f25bb
climbcat <jakob.garde@gmail.com>
2023-07-09 12:01:41 +0200

    use baselayer lib for this project

--- parser/main.cpp
+++ parser/main.cpp
@@ -1,53 +1,22 @@
-#include "lib.c"
+#include <cstdlib>
+#include <cstdio>
+
+#include <cstdint>
+#include <cassert>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/mman.h>
+
+#include "../../baselayer/base.c"
+#include "../../baselayer/memory.c"
+#include "../../baselayer/string.c"
+#include "../../baselayer/utils.c"
+#include "../../baselayer/profile.c"
 
 
 void Test() {
     printf("Running tests ...\n");
-
-    printf("ArenaCreate\n");
-    MArena arena = ArenaCreate();
-    MArena *a = &arena;
-
-    printf("StrLiteral\n");
-    String s1 = StrLiteral(&arena, "hello");
-    String s2 = StrLiteral(&arena, "hello_noteq");
-    String s3 = StrLiteral(&arena, "hello");
-
-    printf("StrPrint - ");
-    StrPrint("%s", s1);
-    printf("\n");
-
-    printf("StrEqual - ");
-    printf("should be (0 1 0): %d %d %d\n", StrEqual(s1, s2), StrEqual(s1, s3), StrEqual(s2, s3));
-
-    printf("StrCat - ");
-    StrPrint("%s", StrCat(&arena, s1, s3));
-    printf("\n");
-
-    printf("StrSplit / StrLstPrint - ");
-    String to_split = StrLiteral(a, "...Hello.I..Have.Been.Split..");
-    printf("splitting: ");
-    StrPrint("%s", to_split);
-    printf(" into: ");
-    StringList *lst = StrSplit(a, to_split, '.');
-    StrLstPrint(lst);
-    printf("\n");
-
-    printf("StrJoin - ");
-    String join = StrJoin(a, lst);
-    StrPrint("%s", join);
-    printf("\n");
-    join = StrJoinInsertChar(a, lst, '/');
-    StrPrint("%s", join);
-    printf("\n");
-
-    printf("CLAInit - ");
-    if (CLAContainsArg("--test", g_argc, g_argv)) {
-        printf("OK");
-    }
-    printf("\n");
 }
-
 
 void ParseInstr(MArena *a, String instr) {
     StrPrint("\n%s\n", instr);
@@ -74,7 +43,9 @@ int main (int argc, char **argv) {
     MArena arena = ArenaCreate();
 
     // load instr
-    String instr = LoadFileMMAP(argv[1]);
+    String instr;
+    instr.str = LoadFileMMAP(argv[1]);
+    instr.len = strlen(instr.str);
     printf("Parsing %s ...\n", argv[1]);
 
     // parse
