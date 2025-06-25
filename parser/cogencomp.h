9c7070cc2da779f8ad7cfe6ba2a1c21aafe034fc
Jakob Garde <jakob@rigsters.com>
2025-06-25 20:36:28 +0200

    apply the StrBuff method to print generated lines to a string buffer; enabling batch work and automated file dumps (tbi)

--- parser/cogencomp.h
+++ parser/cogencomp.h
@@ -2,105 +2,76 @@
 #define __COGENCOMP_H__
 
 
-struct Transform {
-    s32 i;
-};
-struct AComp {
-    Transform *parent;
-    Transform t;
-
-    /*
-    Declare block here
-    */
-};
-AComp AComp_Init() {
-    AComp comp = {};
-    comp.t.i = 1; // identity;
-
-    return comp;
-}
-void AComp_Trace(AComp *comp) {
-
-}
-void AComp_Finally(AComp *comp) {
-
-}
-void AComp_Display(AComp *comp) {
-
-}
-
-
-void PrintStdIncludes() {
-    printf("// standard includes\n");
-    printf("#include <cstdlib>\n");
-    printf("#include <cstring>\n");
-    printf("#include <cstdio>\n");
-    printf("#include <cstddef>\n");
-    printf("#include \"jg_baselayer.h\"\n");
-    printf("#include \"jg_cbui.h\"\n");
-    printf("#include \"simcore.h\"\n");
-    printf("#include \"simlib.h\"\n");
-}
-void PrintDefines(Component *comp) {
+void PrintDefines(StrBuff *b, Component *comp) {
     for (s32 i = 0; i < comp->setting_params.len; ++i) {
         Parameter p = comp->setting_params.arr[i];
-        printf("    #define %.*s comp->%.*s\n", p.name.len, p.name.str, p.name.len, p.name.str);
+        StrBuffPrint1K(b, "    #define %.*s comp->%.*s\n", 4, p.name.len, p.name.str, p.name.len, p.name.str);
     }
-    printf("\n");
+    StrBuffPrint1K(b, "\n", 0);
     for (s32 i = 0; i < comp->declare_members.len; ++i) {
         StructMember m = comp->declare_members.arr[i];
-        printf("    #define %.*s comp->%.*s\n", m.name.len, m.name.str, m.name.len, m.name.str);
+        StrBuffPrint1K(b, "    #define %.*s comp->%.*s\n", 4, m.name.len, m.name.str, m.name.len, m.name.str);
     }
-    // TODO: output, state etc. params
+
+    // TODO: output, state, ..., params
 }
-void PrintUndefs(Component *comp) {
+void PrintUndefs(StrBuff *b, Component *comp) {
     for (s32 i = 0; i < comp->setting_params.len; ++i) {
         Parameter p = comp->setting_params.arr[i];
-        printf("    #undef %.*s\n", p.name.len, p.name.str);
+        StrBuffPrint1K(b, "    #undef %.*s\n", 2, p.name.len, p.name.str);
     }
-    printf("\n");
+    StrBuffPrint1K(b, "\n", 0);
     for (s32 i = 0; i < comp->declare_members.len; ++i) {
         StructMember m = comp->declare_members.arr[i];
-        printf("    #undef %.*s\n", m.name.len, m.name.str);
+        StrBuffPrint1K(b, "    #undef %.*s\n", 2, m.name.len, m.name.str);
     }
-    // TODO: output, state etc. params
+
+    // TODO: output, state, ..., params
 }
 
 
-void ComponentCogen(Component *comp) {
+StrBuff ComponentCogen(Component *comp) {
+    StrBuff buff = StrBuffInit();
+    StrBuff *b = &buff;
 
     // header guard
-    printf("#ifndef __%.*s__\n", comp->type.len, comp->type.str);
-    printf("#define __%.*s__\n", comp->type.len, comp->type.str);
-    printf("\n");
+    StrBuffPrint1K(b, "#ifndef __%.*s__\n", 2, comp->type.len, comp->type.str);
+    StrBuffPrint1K(b, "#define __%.*s__\n", 2, comp->type.len, comp->type.str);
+    StrBuffPrint1K(b, "\n", 0);
 
     // system header
-    PrintStdIncludes();
-    printf("\n");
+    StrBuffPrint1K(b, "// standard includes\n", 0);
+    StrBuffPrint1K(b, "#include <cstdlib>\n", 0);
+    StrBuffPrint1K(b, "#include <cstring>\n", 0);
+    StrBuffPrint1K(b, "#include <cstdio>\n", 0);
+    StrBuffPrint1K(b, "#include <cstddef>\n", 0);
+    StrBuffPrint1K(b, "#include \"jg_baselayer.h\"\n", 0);
+    StrBuffPrint1K(b, "#include \"jg_cbui.h\"\n", 0);
+    StrBuffPrint1K(b, "#include \"simcore.h\"\n", 0);
+    StrBuffPrint1K(b, "#include \"simlib.h\"\n", 0);
+    StrBuffPrint1K(b, "\n", 0);
 
     //
     // share block
 
-    printf("// share block\n\n");
-    StrPrint(comp->share_block);
-    printf("\n\n");
+    StrBuffPrint1K(b, "// share block\n\n", 0);
+    StrBuffAppend(b, comp->share_block);
+    StrBuffPrint1K(b, "\n\n", 0);
 
     //
     // struct
 
-    printf("struct %.*s {\n", comp->type.len, comp->type.str);
-    printf("    Matrix4f t;\n");
-    printf("    Matrix4f *parent;\n");
-    printf("    int index;\n");
-    printf("    char *name;\n");
-    printf("    char *type;\n");
-    printf("    Coords position_absolute;\n");
-    printf("    Coords position_relative;\n");
-    printf("    Rotation rotation_absolute;\n");
-    printf("    Rotation rotation_relative;\n");
-
-    printf("\n");
-    printf("    // parameters\n");
+    StrBuffPrint1K(b, "struct %.*s {\n", 2, comp->type.len, comp->type.str);
+    StrBuffPrint1K(b, "    Matrix4f t;\n", 0);
+    StrBuffPrint1K(b, "    Matrix4f *parent;\n", 0);
+    StrBuffPrint1K(b, "    int index;\n", 0);
+    StrBuffPrint1K(b, "    char *name;\n", 0);
+    StrBuffPrint1K(b, "    char *type;\n", 0);
+    StrBuffPrint1K(b, "    Coords position_absolute;\n", 0);
+    StrBuffPrint1K(b, "    Coords position_relative;\n", 0);
+    StrBuffPrint1K(b, "    Rotation rotation_absolute;\n", 0);
+    StrBuffPrint1K(b, "    Rotation rotation_relative;\n", 0);
+    StrBuffPrint1K(b, "\n    // parameters\n", 0);
 
     // struct parameters
     Str string_s { (char*) "string", 6 };
@@ -109,16 +80,16 @@ void ComponentCogen(Component *comp) {
         Parameter p = comp->setting_params.arr[i];
 
         if (StrEqual(p.type, string_s)) {
-            printf("    char *");
+            StrBuffPrint1K(b, "    char *", 0);
         }
         else if (StrEqual(p.type, vector_s)) {
-            printf("    double ");
+            StrBuffPrint1K(b, "    double ", 0);
         }
         else if (p.type.len) {
-            printf("    %.*s ", p.type.len, p.type.str);
+            StrBuffPrint1K(b, "    %.*s ", 2, p.type.len, p.type.str);
         }
         else {
-            printf("    double ");
+            StrBuffPrint1K(b, "    double ", 0);
         }
 
         if (StrEqual(p.type, vector_s)) {
@@ -130,220 +101,190 @@ void ComponentCogen(Component *comp) {
                     ++cnt;
                 }
             }
-            printf("%.*s[%d]", p.name.len, p.name.str, cnt);
+            StrBuffPrint1K(b, "%.*s[%d]", 3, p.name.len, p.name.str, cnt);
         }
         else {
-            printf("%.*s", p.name.len, p.name.str);
+            StrBuffPrint1K(b, "%.*s", 2, p.name.len, p.name.str);
         }
 
         if (p.default_val.len) {
             if (StrEqual(p.type, string_s)) {
-                printf(" = (char*) %.*s", p.default_val.len, p.default_val.str);
+                StrBuffPrint1K(b, " = (char*) %.*s", 2, p.default_val.len, p.default_val.str);
             }
             else {
-                printf(" = %.*s", p.default_val.len, p.default_val.str);
+                StrBuffPrint1K(b, " = %.*s", 2, p.default_val.len, p.default_val.str);
             }
         }
-        printf(";\n");
+        StrBuffPrint1K(b, ";\n", 0);
     }
 
     // declare members
-    printf("\n    // declares\n");
+    StrBuffPrint1K(b, "\n    // declares\n", 0);
     for (s32 i = 0; i < comp->declare_members.len; ++i) {
         StructMember m = comp->declare_members.arr[i];
 
-        printf("    %.*s ", m.type.len, m.type.str);
+        StrBuffPrint1K(b, "    %.*s ", 2, m.type.len, m.type.str);
         if (m.is_pointer_type) {
-            printf("*");
+            StrBuffPrint1K(b, "*", 0);
         }
-        printf("%.*s", m.name.len, m.name.str);
+        StrBuffPrint1K(b, "%.*s", 2, m.name.len, m.name.str);
 
         if (m.is_array_type) {
-            printf("[%d]", m.array_type_sz);
+            StrBuffPrint1K(b, "[%d]", 1, m.array_type_sz);
         }
 
         if (m.defval.len) {
-            printf(" = %.*s", m.defval.len, m.defval.str);
+            StrBuffPrint1K(b, " = %.*s", 2, m.defval.len, m.defval.str);
         }
-        printf(";\n");
+        StrBuffPrint1K(b, ";\n", 0);
     }
-    printf("};\n\n");
-
+    StrBuffPrint1K(b, "};\n\n", 0);
 
     //
     //  Init
 
-    printf("%.*s Init_%.*s(s32 index, char *name, Instrument *instrument) {\n", comp->type.len, comp->type.str, comp->type.len, comp->type.str);
-    printf("    %.*s _comp = {};\n", comp->type.len, comp->type.str);
-    printf("    %.*s *comp = &_comp;\n", comp->type.len, comp->type.str);
-    printf("    comp->type = (char*) \"%.*s\";\n", comp->type.len, comp->type.str);
-    printf("    comp->name = name;\n");
-    printf("\n");
+    StrBuffPrint1K(b, "%.*s Init_%.*s(s32 index, char *name, Instrument *instrument) {\n", 4, comp->type.len, comp->type.str, comp->type.len, comp->type.str);
+    StrBuffPrint1K(b, "    %.*s _comp = {};\n", 2, comp->type.len, comp->type.str);
+    StrBuffPrint1K(b, "    %.*s *comp = &_comp;\n", 2, comp->type.len, comp->type.str);
+    StrBuffPrint1K(b, "    comp->type = (char*) \"%.*s\";\n", 2, comp->type.len, comp->type.str);
+    StrBuffPrint1K(b, "    comp->name = name;\n", 0);
+    StrBuffPrint1K(b, "\n", 0);
     if (comp->initalize_block.len) {
-        PrintDefines(comp);
-        printf("    // ---------------------------\n\n");
+        PrintDefines(b, comp);
+        StrBuffPrint1K(b, "    ////////////////////////////////////////////////////////////////\n\n", 0);
 
-        StrPrint(comp->initalize_block);
+        StrBuffAppend(b, comp->initalize_block);
 
-        printf("\n    // ---------------------------\n");
-        PrintUndefs(comp);
-        printf("\n");
+        StrBuffPrint1K(b, "\n\n    ////////////////////////////////////////////////////////////////\n", 0);
+        PrintUndefs(b, comp);
+        StrBuffPrint1K(b, "\n", 0);
     }
-    printf("    return _comp;\n");
-    printf("}\n\n");
-
-
+    StrBuffPrint1K(b, "    return _comp;\n", 0);
+    StrBuffPrint1K(b, "}\n\n", 0);
 
     //
     //  Trace
 
-    printf("void Trace_%.*s(%.*s *comp, Neutron *particle, Instrument *instrument) {\n", comp->type.len, comp->type.str, comp->type.len, comp->type.str);
+    StrBuffPrint1K(b, "void Trace_%.*s(%.*s *comp, Neutron *particle, Instrument *instrument) {\n", 4, comp->type.len, comp->type.str, comp->type.len, comp->type.str);
     if (comp->trace_block.len) {
-        printf("#define x particle->x\n");
-        printf("#define y particle->y\n");
-        printf("#define z particle->z\n");
-        printf("#define vx particle->vx\n");
-        printf("#define vy particle->vy\n");
-        printf("#define vz particle->vz\n");
-        printf("#define sx particle->sx\n");
-        printf("#define sy particle->sy\n");
-        printf("#define sz particle->sz\n");
-        printf("#define t particle->t\n");
-        printf("#define p particle->p\n");
+        StrBuffPrint1K(b, "    #define x particle->x\n", 0);
+        StrBuffPrint1K(b, "    #define y particle->y\n", 0);
+        StrBuffPrint1K(b, "    #define z particle->z\n", 0);
+        StrBuffPrint1K(b, "    #define vx particle->vx\n", 0);
+        StrBuffPrint1K(b, "    #define vy particle->vy\n", 0);
+        StrBuffPrint1K(b, "    #define vz particle->vz\n", 0);
+        StrBuffPrint1K(b, "    #define sx particle->sx\n", 0);
+        StrBuffPrint1K(b, "    #define sy particle->sy\n", 0);
+        StrBuffPrint1K(b, "    #define sz particle->sz\n", 0);
+        StrBuffPrint1K(b, "    #define t particle->t\n", 0);
+        StrBuffPrint1K(b, "    #define p particle->p\n", 0);
 
-        printf("\n");
-        PrintDefines(comp);
-        printf("    // ---------------------------\n\n");
+        StrBuffPrint1K(b, "\n", 0);
+        PrintDefines(b, comp);
+        StrBuffPrint1K(b, "    ////////////////////////////////////////////////////////////////\n\n", 0);
 
-        StrPrint(comp->trace_block);
+        StrBuffAppend(b, comp->trace_block);
 
-        printf("\n    // ---------------------------\n");
-        PrintUndefs(comp);
-        printf("#undef x\n");
-        printf("#undef y\n");
-        printf("#undef z\n");
-        printf("#undef vx\n");
-        printf("#undef vy\n");
-        printf("#undef vz\n");
-        printf("#undef sx\n");
-        printf("#undef sy\n");
-        printf("#undef sz\n");
-        printf("#undef t\n");
-        printf("#undef p\n");
+        StrBuffPrint1K(b, "\n\n    ////////////////////////////////////////////////////////////////\n", 0);
+        PrintUndefs(b, comp);
+        StrBuffPrint1K(b, "\n", 0);
+
+        StrBuffPrint1K(b, "    #undef x\n", 0);
+        StrBuffPrint1K(b, "    #undef y\n", 0);
+        StrBuffPrint1K(b, "    #undef z\n", 0);
+        StrBuffPrint1K(b, "    #undef vx\n", 0);
+        StrBuffPrint1K(b, "    #undef vy\n", 0);
+        StrBuffPrint1K(b, "    #undef vz\n", 0);
+        StrBuffPrint1K(b, "    #undef sx\n", 0);
+        StrBuffPrint1K(b, "    #undef sy\n", 0);
+        StrBuffPrint1K(b, "    #undef sz\n", 0);
+        StrBuffPrint1K(b, "    #undef t\n", 0);
+        StrBuffPrint1K(b, "    #undef p\n", 0);
     }
-    printf("}\n\n");
+    StrBuffPrint1K(b, "}\n\n", 0);
+
 
     //
     //  Save
 
-    printf("void Save_%.*s(%.*s *comp) {\n", comp->type.len, comp->type.str, comp->type.len, comp->type.str);
-    printf("\n");
+    StrBuffPrint1K(b, "void Save_%.*s(%.*s *comp) {\n", 4, comp->type.len, comp->type.str, comp->type.len, comp->type.str);
+    StrBuffPrint1K(b, "\n", 0);
     if (comp->save_block.len) {
-        PrintDefines(comp);
-        printf("    // ---------------------------\n\n");
+        PrintDefines(b, comp);
+        StrBuffPrint1K(b, "    ////////////////////////////////////////////////////////////////\n\n", 0);
 
-        StrPrint(comp->save_block);
+        StrBuffAppend(b, comp->save_block);
 
-        printf("\n    // ---------------------------\n");
-        PrintUndefs(comp);
+        StrBuffPrint1K(b, "\n\n    ////////////////////////////////////////////////////////////////\n", 0);
+        PrintUndefs(b, comp);
     }
-    printf("}\n\n");
+    StrBuffPrint1K(b, "}\n\n", 0);
 
     //
     //  Finally
 
-    printf("void Finally_%.*s(%.*s *comp) {\n", comp->type.len, comp->type.str, comp->type.len, comp->type.str);
-    printf("\n");
+    StrBuffPrint1K(b, "void Finally_%.*s(%.*s *comp) {\n", 4, comp->type.len, comp->type.str, comp->type.len, comp->type.str);
+    StrBuffPrint1K(b, "\n", 0);
     if (comp->finally_block.len) {
-        PrintDefines(comp);
-        printf("    // ---------------------------\n\n");
+        PrintDefines(b, comp);
+        StrBuffPrint1K(b, "    ////////////////////////////////////////////////////////////////\n\n", 0);
 
-        StrPrint(comp->finally_block);
+        StrBuffAppend(b, comp->finally_block);
 
-        printf("\n    // ---------------------------\n");
-        PrintUndefs(comp);
+        StrBuffPrint1K(b, "\n\n    ////////////////////////////////////////////////////////////////\n", 0);
+        PrintUndefs(b, comp);
     }
-    printf("}\n\n");
+    StrBuffPrint1K(b, "}\n\n", 0);
 
     //
     //  Display
 
-    printf("void Display_%.*s(%.*s *comp) {\n", comp->type.len, comp->type.str, comp->type.len, comp->type.str);
+    StrBuffPrint1K(b, "void Display_%.*s(%.*s *comp) {\n", 4, comp->type.len, comp->type.str, comp->type.len, comp->type.str);
     if (comp->display_block.len) {
-        printf("#define magnify mcdis_magnify\n");
-        printf("#define line mcdis_line\n");
-        printf("#define dashed_line mcdis_dashed_line\n");
-        printf("#define multiline mcdis_multiline\n");
-        printf("#define rectangle mcdis_rectangle\n");
-        printf("#define box mcdis_box\n");
-        printf("#define circle mcdis_circle\n");
-        printf("#define Circle mcdis_Circle\n");
-        printf("#define cylinder mcdis_cylinder\n");
-        printf("#define cone mcdis_cone\n");
-        printf("#define sphere mcdis_sphere\n");
-        printf("\n");
+        StrBuffPrint1K(b, "    #define magnify mcdis_magnify\n", 0);
+        StrBuffPrint1K(b, "    #define line mcdis_line\n", 0);
+        StrBuffPrint1K(b, "    #define dashed_line mcdis_dashed_line\n", 0);
+        StrBuffPrint1K(b, "    #define multiline mcdis_multiline\n", 0);
+        StrBuffPrint1K(b, "    #define rectangle mcdis_rectangle\n", 0);
+        StrBuffPrint1K(b, "    #define box mcdis_box\n", 0);
+        StrBuffPrint1K(b, "    #define circle mcdis_circle\n", 0);
+        StrBuffPrint1K(b, "    #define Circle mcdis_Circle\n", 0);
+        StrBuffPrint1K(b, "    #define cylinder mcdis_cylinder\n", 0);
+        StrBuffPrint1K(b, "    #define cone mcdis_cone\n", 0);
+        StrBuffPrint1K(b, "    #define sphere mcdis_sphere\n", 0);
 
-        PrintDefines(comp);
-        printf("    // ---------------------------\n\n");
+        StrBuffPrint1K(b, "\n", 0);
+        PrintDefines(b, comp);
+        StrBuffPrint1K(b, "    ////////////////////////////////////////////////////////////////\n\n", 0);
 
-        StrPrint(comp->display_block);
+        StrBuffAppend(b, comp->display_block);
 
-        printf("\n    // ---------------------------\n");
-        PrintUndefs(comp);
-        printf("\n");
-        printf("#undef magnify\n");
-        printf("#undef line\n");
-        printf("#undef dashed_line\n");
-        printf("#undef multiline\n");
-        printf("#undef rectangle\n");
-        printf("#undef box\n");
-        printf("#undef circle\n");
-        printf("#undef Circle\n");
-        printf("#undef cylinder\n");
-        printf("#undef cone\n");
-        printf("#undef sphere\n");
+        StrBuffPrint1K(b, "\n\n    ////////////////////////////////////////////////////////////////\n", 0);
+        PrintUndefs(b, comp);
+        StrBuffPrint1K(b, "\n", 0);
+
+        StrBuffPrint1K(b, "    #undef magnify\n", 0);
+        StrBuffPrint1K(b, "    #undef line\n", 0);
+        StrBuffPrint1K(b, "    #undef dashed_line\n", 0);
+        StrBuffPrint1K(b, "    #undef multiline\n", 0);
+        StrBuffPrint1K(b, "    #undef rectangle\n", 0);
+        StrBuffPrint1K(b, "    #undef box\n", 0);
+        StrBuffPrint1K(b, "    #undef circle\n", 0);
+        StrBuffPrint1K(b, "    #undef Circle\n", 0);
+        StrBuffPrint1K(b, "    #undef cylinder\n", 0);
+        StrBuffPrint1K(b, "    #undef cone\n", 0);
+        StrBuffPrint1K(b, "    #undef sphere\n", 0);
     }
-    printf("}\n\n");
+    StrBuffPrint1K(b, "}\n\n", 0);
 
 
     // close header guard
-    printf("#endif\n");
+    StrBuffPrint1K(b, "#endif\n", 0);
 
-    /*
-    What's the cogen gonna be: Using the example component "AComp.comp"
-    - base functions: AComp.h 
-    - shared library: AComp_share.h - all of the share block, which can be a lot of code
-    - meta/reflection: AComp_meta.h - component type name, parameter type info/names, doc strings
-            which is all of the stuff that can be provided through cogen - now that we are at it.
-            With this info, we can build UI, get function pointers for the type-agnostic simulation core.
+    // print it all
+    StrPrint(b->GetStr());
 
-    Let's focus on AComp.h for now. We would provide the following:
-    - header guard
-    - include any AComp_share.h file
-    - component struct: declare block (parsed for symbol mapping)
-    - component struct: Transform t; Transform *parent; comp_type_name, comp_name (instance name)
-    - symbols: define each declare block sumbols as #define sym comp->sym - for the entire file ! 
-
-    - AComp AComp_Init() {} returning a component struct; runs init code and returns
-    - void Trace(Acomp *comp) {}
-    - Finally
-    - Display
-
-    - un-define symbols
-    - end header guard
-
-    The AComp_share.h file should contain the following:
-    - header guard
-    - share code block copy-pasted verbatim into the file 
-    - end header guard
-
-    The AComp_meta.h file should contain some of the following things (less specific/brain-stormy)
-    - header guard
-    - include the AComp.h file
-    - void GetCompSignatures(HashMap dest_map) - returns function pointers to trace, finally and display
-    - Wrapper functions that take and return void* arguments? 
-    - ...
-    */
+    return buff;
 }
 
 
